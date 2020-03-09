7d9af150d672da7b2316a3859226063a87bcc76b
Adrian Szlegel <adrian@oaknode.com>
2020-03-09 12:18:25 +0100

    Gitignore plus quick test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('first render', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const firstHeader = getByText(/React based SW battle app/i);
+  const secondHeader = getByText(/Select game type:/i);
+  const firstRadio = getByText(/People/i);
+  const secondRadio = getByText(/Starhips/i);
+  expect(firstHeader).toBeInTheDocument();
+  expect(secondHeader).toBeInTheDocument();
+  expect(secondHeader).toBeInTheDocument();
+  expect(firstRadio).toBeInTheDocument();
+  expect(secondRadio).toBeInTheDocument();
 });
