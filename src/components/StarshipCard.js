7dc1e8d01f723329ea609a5962caf13b95da3143
Adrian Szlegel <adrian@oaknode.com>
2020-04-10 18:47:29 +0200

    Starship Card splitted from the main code and changed to function component

--- src/components/StarshipCard.js
+++ src/components/StarshipCard.js
@@ -4,39 +4,47 @@ import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import Typography from '@material-ui/core/Typography';
 
-class StarshipCard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
-    render() {
-        return(
-            <Card className={ this.props.resultClass }>
-                <CardHeader 
-                    title={this.props.name}
-                    subheader="Starship name" 
-                />
-                <CardContent>
-                    <Typography variant="h5" component="h2">Starship Details:</Typography>
-                    <Typography variant="body2" component="p">
-                        Model: <strong>{this.props.model}</strong><br />
-                        Manufacturer: <strong>{this.props.manufacturer}</strong><br />
-                        Cost In Credits: <strong>{this.props.costInCredits}</strong><br />
-                        Length: <strong>{this.props.length}</strong><br />
-                        Max Atmosphering Speed: <strong>{this.props.maxAtmospheringSpeed}</strong><br />
-                        Crew: <strong>{this.props.crew}</strong><br />
-                        Passengers: <strong>{this.props.passengers}</strong><br />
-                        Cargo Capacity: <strong>{this.props.cargoCapacity}</strong><br />
-                        Consumables: <strong>{this.props.consumables}</strong><br />
-                        Hyperdrive Rating: <strong>{this.props.hyperdriveRating}</strong><br />
-                        MGLT: <strong>{this.props.mglt}</strong><br />
-                        Starship Class: <strong>{this.props.starshipClass}</strong>
-                    </Typography>
-                </CardContent>
-            </Card>
-        );
-    }
+function StarshipCard(props) {
+    const { 
+        name,
+        model, 
+        manufacturer, 
+        cost_in_credits, 
+        length, 
+        max_atmosphering_speed, 
+        crew, 
+        passengers, 
+        cargo_capacity,
+        consumables,
+        hyperdrive_rating,
+        MGLT,
+        starship_class 
+    } = props.player;
+    return(
+        <Card className={ props.resultClass }>
+            <CardHeader 
+                title={name}
+                subheader="Starship name" 
+            />
+            <CardContent>
+                <Typography variant="h5" component="h2">Starship Details:</Typography>
+                <Typography variant="body2" component="p">
+                    Model: <strong>{model}</strong><br />
+                    Manufacturer: <strong>{manufacturer}</strong><br />
+                    Cost In Credits: <strong>{cost_in_credits}</strong><br />
+                    Length: <strong>{length}</strong><br />
+                    Max Atmosphering Speed: <strong>{max_atmosphering_speed}</strong><br />
+                    Crew: <strong>{crew}</strong><br />
+                    Passengers: <strong>{passengers}</strong><br />
+                    Cargo Capacity: <strong>{cargo_capacity}</strong><br />
+                    Consumables: <strong>{consumables}</strong><br />
+                    Hyperdrive Rating: <strong>{hyperdrive_rating}</strong><br />
+                    MGLT: <strong>{MGLT}</strong><br />
+                    Starship Class: <strong>{starship_class}</strong>
+                </Typography>
+            </CardContent>
+        </Card>
+    );
 }
 
 export default StarshipCard;
